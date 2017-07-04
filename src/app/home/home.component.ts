51f73625f5da233f94f7009211ee00e86b56db82
imrebartis <imbartis@gmail.com>
2017-07-04 14:59:17 +0300

    home.component.ts: imported eventService & event, defined events as an Array<Event>, updated constructor & ngOnInit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { EventService } from '../event.service';
+import { Event } from '../event';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  events: Array<Event>;
+
+  constructor(private _eventService: EventService) { }
 
   ngOnInit() {
+    this._eventService.getEvents()
+      .subscribe(res=> this.events = res);
   }
 
 }
