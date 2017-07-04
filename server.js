65e6941877afcb1691b251c8eb7e0a0e30c6a515
imrebartis <imbartis@gmail.com>
2017-07-04 12:28:16 +0300

    server.js file created

---
+++ server.js
@@ -0,0 +1,30 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const http = require('http');
+const app = express();
+
+//Parsers
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended : false }));
+
+//Serve static files
+app.use(express.static(path.join(_dirname, 'dist')));
+
+//Set our api routes
+// app.use('/api', api);
+
+//Return other routes to Angular index file
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+//Set port
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+//Create the http server
+const server = http.createServer(app);
+
+server.listen(port, () => console.log('Running in localhost: ${port}'));
