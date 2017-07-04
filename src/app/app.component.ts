0f79d7b4c4ad306d19c75f267d4c4f0e33b9d0fb
imrebartis <imbartis@gmail.com>
2017-07-04 14:20:24 +0300

    styles added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = `<div class="icon-bar five-up">
+      <a class="item" ui-sref="eventList">
+          <i class="fi-list-thumbnails"></i>
+          <label>Events List</label>
+      </a>
+      <a class="item" ui-sref="addEvent">
+          <i class="fi-plus"></i>
+          <label>Add Event</label>
+      </a>
+      <a class="item" ui-sref="about">
+          <i class="fi-info"></i>
+          <label>About</label>
+      </a>
+  </div>`;
 }
