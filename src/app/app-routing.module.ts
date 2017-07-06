7a5bb4d5a8d359b9e9559aa9e862037b8024e11d
imrebartis <imbartis@gmail.com>
2017-07-06 09:49:12 +0300

    imported event component and added its path in app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+import { EventComponent } from './event/event.component';
 
 const routes: Routes = [
 
     {path: '', component: HomeComponent },
-    {path: 'details/:id', component: DetailsComponent }
+    {path: 'details/:id', component: DetailsComponent },
+    {path: 'event', component: EventComponent }
 
 ];
 
