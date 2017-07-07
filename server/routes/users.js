ae408e92dfa0df9c7cae850c621ba674e6fe7e27
imrebartis <imbartis@gmail.com>
2017-07-07 09:35:51 +0300

    config folder added for passport auth

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const User = require('../model/user');
+const User = require('../models/user');
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
